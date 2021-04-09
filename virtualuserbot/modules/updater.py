3d4d638cfef860f3007ce4722148ff1e079ff97d
InukaAsith <inukaasith7@gmail.com>
2021-04-09 02:07:38 +0000

    pylint: auto fixes

--- virtualuserbot/modules/updater.py
+++ virtualuserbot/modules/updater.py
@@ -16,9 +16,7 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "please check out to an official branch, and re-start the updater."
 )
 OFFICIAL_UPSTREAM_REPO = Config.UPSTREAM_REPO
-BOT_IS_UP_TO_DATE = (
-    "your userbot >> is up-to-date<< \nThanks For Using PermanUserbot."
-)
+BOT_IS_UP_TO_DATE = "your userbot >> is up-to-date<< \nThanks For Using PermanUserbot."
 NEW_BOT_UP_DATE_FOUND = (
     "New Update Found For {branch_name}\n"
     "ChangeLog: \n\n{changelog}\n"
